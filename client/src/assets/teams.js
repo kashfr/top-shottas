c36564580de11d2f4e15535143880cf6c8d5f142
Kaheem Freeman <kfreeman@Kaheems-MacBook-Pro-2.local>
2021-12-03 15:22:58 -0500

    finished adding logos

--- client/src/assets/teams.js
+++ client/src/assets/teams.js
@@ -1,5 +1,10 @@
 export const teams = [
   {
+    name: "NBA",
+    image:
+      "https://content.sportslogos.net/logos/6/982/full/8147__national_basketball_association-primary-2018.png",
+  },
+  {
     name: "St. Louis Hawks",
     image:
       "https://content.sportslogos.net/logos/6/220/full/8190_atlanta_hawks-primary-2021.png",
@@ -15,87 +20,143 @@ export const teams = [
       "https://content.sportslogos.net/logos/6/213/full/boston_celtics_logo_primary_19977628.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Brooklyn Nets",
+    image:
+      "https://content.sportslogos.net/logos/6/3786/full/137_brooklyn-nets-primary-2013.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Charlotte Hornets",
+    image:
+      "https://content.sportslogos.net/logos/6/5120/full/1926_charlotte__hornets_-primary-2015.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Chicago Bulls",
+    image:
+      "https://content.sportslogos.net/logos/6/221/full/zdrycpc7mh5teihl10gko8sgf.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Cleveland Cavaliers",
+    image:
+      "https://content.sportslogos.net/logos/6/222/full/cleveland_cavaliers_logo_primary_20187997.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Dallas Mavericks",
+    image:
+      "https://content.sportslogos.net/logos/6/228/full/3463_dallas_mavericks-primary-2018.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Denver Nuggets",
+    image:
+      "https://content.sportslogos.net/logos/6/229/full/8926_denver_nuggets-primary-2019.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Detroit Pistons",
+    image:
+      "https://content.sportslogos.net/logos/6/223/full/2164_detroit_pistons-primary-2018.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Golden State Warriors",
+    image:
+      "https://content.sportslogos.net/logos/6/235/full/3152_golden_state_warriors-primary-2020.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Houston Rockets",
+    image:
+      "https://content.sportslogos.net/logos/6/230/full/6830_houston_rockets-primary-2020.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Indiana Pacers",
+    image:
+      "https://content.sportslogos.net/logos/6/224/full/4812_indiana_pacers-primary-2018.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Los Angeles Clippers",
+    image:
+      "https://content.sportslogos.net/logos/6/236/full/los_angeles_clippers_logo_primary_2019_sportslogosnet-3776.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Los Angeles Lakers",
+    image:
+      "https://content.sportslogos.net/logos/6/237/full/uig7aiht8jnpl1szbi57zzlsh.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Memphis Grizzlies",
+    image:
+      "https://content.sportslogos.net/logos/6/231/full/4373_memphis_grizzlies-primary-2019.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Miami Heat",
+    image:
+      "https://content.sportslogos.net/logos/6/214/full/burm5gh2wvjti3xhei5h16k8e.gif",
   },
   {
-    name: "",
-    image: "",
+    name: "Milwaukee Bucks",
+    image:
+      "https://content.sportslogos.net/logos/6/225/full/milwaukee_bucks_logo_primary_20165763.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Minnesota Timberwolves",
+    image:
+      "https://content.sportslogos.net/logos/6/232/full/9669_minnesota_timberwolves-primary-2018.png",
   },
   {
-    name: "",
-    image: "",
+    name: "New Orleans Pelicans",
+    image:
+      "https://content.sportslogos.net/logos/6/4962/full/2681_new_orleans_pelicans-primary-2014.png",
   },
   {
-    name: "",
-    image: "",
+    name: "New York Knicks",
+    image:
+      "https://content.sportslogos.net/logos/6/216/full/2nn48xofg0hms8k326cqdmuis.gif",
   },
   {
-    name: "",
-    image: "",
+    name: "Oklahoma City Thunder",
+    image:
+      "https://content.sportslogos.net/logos/6/2687/full/khmovcnezy06c3nm05ccn0oj2.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Orlando Magic",
+    image:
+      "https://content.sportslogos.net/logos/6/217/full/orlando_magic_logo_primary_20117178.png",
   },
   {
-    name: "",
-    image: "",
+    name: "Philadelphia 76ers",
+    image:
+      "https://content.sportslogos.net/logos/6/218/full/7034_philadelphia_76ers-primary-2016.png",
+  },
+  {
+    name: "Phoenix Suns",
+    image:
+      "https://content.sportslogos.net/logos/6/238/full/phoenix_suns_logo_primary_20143696.png",
+  },
+  {
+    name: "Portland Trail Blazers",
+    image:
+      "https://content.sportslogos.net/logos/6/239/full/9725_portland_trail_blazers-primary-2018.png",
+  },
+  {
+    name: "Sacramento Kings",
+    image:
+      "https://content.sportslogos.net/logos/6/240/full/4043_sacramento_kings-primary-2017.png",
+  },
+  {
+    name: "San Antonio Spurs",
+    image:
+      "https://content.sportslogos.net/logos/6/233/full/2547_san_antonio_spurs-primary-2018.png",
+  },
+  {
+    name: "Toronto Raptors",
+    image:
+      "https://content.sportslogos.net/logos/6/227/full/7024_toronto_raptors-primary-2021.png",
+  },
+  {
+    name: "Utah Jazz",
+    image:
+      "https://content.sportslogos.net/logos/6/234/full/6749_utah_jazz-primary-2017.png",
+  },
+  {
+    name: "Washington Wizards",
+    image:
+      "https://content.sportslogos.net/logos/6/219/full/5671_washington_wizards-primary-2016.png",
   },
 ];
